6b5de89693a67bcf110f40e75632083f6a841f32
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-25 10:27:31 +0100

    add navbar and daisyUI

--- src/app/(business)/layout.tsx
+++ src/app/(business)/layout.tsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
+
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-     <nav>Home</nav>
+      <nav className="navbar bg-violet-50">
+        <div className="flex-1">
+          <Link className="btn btn-ghost text-lg p-1 font-bold" href=".">
+            Includio
+          </Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li className="px-2">
+              <Link href="\chat\">Businesses</Link>
+            </li>
+            <li>
+              <Link href="\statistics\">Index</Link>
+            </li>
+            <li>
+              <Link href="/">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <main>{children}</main>
     </>
   );
