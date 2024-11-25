c0b202aa81da8d5c8530230b896bb26b06611e78
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-25 14:21:48 +0100

    make it responsive

--- src/features/business/components/buisness-card.tsx
+++ src/features/business/components/buisness-card.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export function BusinessCard() {
   return (
-    <div className="card card-side bg-base-100 shadow-xl mt-10 max-w-4xl">
+    <div className="card md:card-side bg-base-100 shadow-xl mt-10 max-w-4xl flex flex-col justify-center items-center w-10/12">
       <Image
         src="/restaurant1.png"
         width={300}
         height={400}
         alt="Picture of the slow chat application"
-        className="pt-0 mt-6 md:mt-0 shadow-2xl rounded-lg"
+        className="pt-0 md:mt-0 shadow-2xl rounded-t-lg md:rounded-tr-none md:rounded-l-lg  w-full"
       />
       <div className="card-body">
         <h2 className="card-title">Harmony Bites</h2>
@@ -18,8 +18,10 @@ export function BusinessCard() {
           every dish is a celebration of taste and creativity. Perfect for
           casual dinners or special occasions.
         </p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-secondary h-14 text-base rounded-lg">Read More</button>
+        <div className="card-actions justify-end mt-3">
+          <button className="btn btn-secondary h-14 text-base rounded-lg">
+            Read More
+          </button>
         </div>
       </div>
     </div>
---
+++ src/features/business/components/nav.tsx
@@ -0,0 +1,64 @@
+import Link from "next/link";
+
+export function Nav(){
+ return (
+   <nav className="navbar bg-green-50">
+     <div className="flex-1">
+       <Link className="text-lg font-bold" href=".">
+         Includio
+       </Link>
+     </div>
+     <div className="flex-none">
+       <ul className="hidden md:flex menu menu-horizontal px-1 font-bold">
+         <li className="px-2">
+           <Link className="hover:bg-green-100 rounded-lg" href="\businesses\">
+             Businesses
+           </Link>
+         </li>
+         <li>
+           <Link className="hover:bg-green-100 rounded-lg" href="/">
+             Index
+           </Link>
+         </li>
+         <li>
+           <Link className="hover:bg-green-100 rounded-lg" href="/">
+             Contact
+           </Link>
+         </li>
+       </ul>
+       <div className="block md:hidden dropdown">
+         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+           <svg
+             xmlns="http://www.w3.org/2000/svg"
+             className="h-5 w-5"
+             fill="none"
+             viewBox="0 0 24 24"
+             stroke="currentColor"
+           >
+             <path
+               strokeLinecap="round"
+               strokeLinejoin="round"
+               strokeWidth="2"
+               d="M4 6h16M4 12h16M4 18h7"
+             />
+           </svg>
+         </div>
+         <ul
+           tabIndex={0}
+           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 left-auto right-0 shadow"
+         >
+           <li>
+             <Link href="\businesses\">Businesses</Link>
+           </li>
+           <li>
+             <Link href="/">Index</Link>
+           </li>
+           <li>
+             <Link href="/">Contact</Link>
+           </li>
+         </ul>
+       </div>
+     </div>
+   </nav>
+ );
+}
