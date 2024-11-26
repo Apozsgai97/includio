463053e1ef65b26754a80b9a391f8088f94676dd
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-26 16:38:58 +0100

    add more styling

--- src/app/(business)/businesses/[id]/page.tsx
+++ src/app/(business)/businesses/[id]/page.tsx
@@ -5,8 +5,8 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   console.log(id);
 
   return (
-    <div className="container mx-auto max-w-4xl rounded-lg shadow-md md:m-6 pb-6">
-      <section className="flex flex-col sm:flex-row items-center gap-8 mb-6 bg-emerald-50 pb-6 md:pb-0 rounded-t-lg">
+    <div className="container mx-auto max-w-4xl rounded-lg shadow-md md:m-6 mb-6">
+      <section className="flex flex-col sm:flex-row items-center gap-8 pb-6 md:pb-0 rounded-t-lg bg-emerald-50">
         <Image
           src="/restaurant1.png"
           width={300}
@@ -20,8 +20,8 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         </div>
       </section>
 
-      <section className="flex flex-col lg:flex-row gap-6">
-        <article className="bg-white max-w-96 md:w-96">
+      <section className="flex flex-col lg:flex-row gap-6 py-6">
+        <article className="max-w-96 md:w-96 ">
           <h2 className="text-2xl font-semibold text-gray-800 mb-2 pl-4">
             Location
           </h2>
@@ -30,27 +30,30 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
           </p>
           <div className="w-full h-64 overflow-hidden shadow-md">
             <iframe
-              src="https://www.google.com/maps/embed?pb=YOUR_GOOGLE_MAPS_EMBED_LINK"
-              width="100%"
-              height="100%"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1140.631670099329!2d18.009561359512706!3d59.33756915057438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d1ad188b8f3%3A0xf92c2f046e1956f2!2sSALT%20%F0%9F%A7%82%20School%20of%20Applied%20Technology!5e0!3m2!1ssv!2sse!4v1732633301034!5m2!1ssv!2sse"
+              width="600"
+              height="450"
               style={{ border: 0 }}
               loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              title="Google Map"
             ></iframe>
           </div>
         </article>
-
-        <article className="flex flex-col flex-1 bg-white justify-between md:px-12 p-4 md:py-0">
-         <div>
-          <h2 className="text-2xl font-semibold text-gray-800 mb-2">
-            About Us
-          </h2>
-          <p className="text-gray-700 mb-4">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, id quibusdam nobis incidunt, dignissimos veniam tempora corporis similique ratione nesciunt cum magnam culpa perferendis necessitatibus quam quo. Temporibus, laborum laudantium.
-          </p>
-         </div>
-          <div className="flex flex-col md:flex-row items-start md:items-center justify-around bg-slate-50 p-3 rounded-lg shadow-sm">
+        <article className="flex flex-col flex-1 justify-start md:px-12 p-4 md:py-0">
+          <div>
+            <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+              About Us
+            </h2>
+            <p className="text-gray-700 mb-6">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque,
+              id quibusdam nobis incidunt, dignissimos veniam tempora corporis
+              similique ratione nesciunt cum magnam culpa perferendis
+              necessitatibus quam quo. Temporibus, laborum laudantium.
+            </p>
+          </div>
+          <div className="flex flex-col items-start justify-around">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+              Contact
+            </h2>
             <p className="text-gray-700 flex items-center gap-2">
               📧 example@email.com
             </p>
@@ -59,6 +62,20 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
             </p>
           </div>
         </article>
+      </section>
+      <section className=" flex flex-col justify-center items-center p-8 bg-emerald-50">
+        <div className="w-11/12 shadow-md p-6 flex flex-col justify-center items-center rounded-lg bg-white">
+        <h2 className="text-3xl font-bold text-gray-800 text-center">
+          Accessibility
+        </h2>
+        <article className="stats shadow my-8 w-44 border-2 border-accent">
+          <div className="stat text-center p-1">
+            <div className="stat-title">Score</div>
+            <div className="stat-value">0</div>
+          </div>
+        </article>
+        <article></article>
+        </div>
       </section>
     </div>
   );
